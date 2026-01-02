37153670b829216cd0cdfa6d999745bd66143cec
Dom Molloy <67226602+dom-molloy@users.noreply.github.com>
2026-01-01 19:53:23 -0700

    Refactor import statements for better readability

--- seeus_mvp/app.py
+++ seeus_mvp/app.py
@@ -5,34 +5,40 @@ try:
 except Exception:
     pass
 
-import os
-import json
-import uuid
-from datetime import datetime
-from urllib.parse import urlencode
-
-import streamlit as st
-
-from growth_ui import render_growth_dashboard
 from question_store import load_question_bank
 
 from db import (
-    save_report, get_latest_report,
-    init_db, upsert_user, create_relationship, list_relationships, get_relationship,
-    create_session, get_open_session, end_session, save_response,
-    get_answers_for_session, get_last_answers, get_answer_history,
-    create_invite, get_invite, mark_invite_used,
-    archive_relationship, restore_relationship,
+    save_report,
+    get_latest_report,
+    init_db,
+    upsert_user,
+    create_relationship,
+    list_relationships,
+    get_relationship,
+    create_session,
+    get_open_session,
+    end_session,
+    save_response,
+    get_answers_for_session,
+    get_last_answers,
+    get_answer_history,
+    create_invite,
+    get_invite,
+    mark_invite_used,
+    archive_relationship,
+    restore_relationship,
 )
 
-from scoring import score_solo, score_duo, overall_score
-from llm_scoring import score_duo_llm, overall_from_llm
-from reporting import DIMENSION_ORDER, DIMENSION_LABELS, build_headlines
-from deep_research import run_deep_research
-from research_packet import build_key_quotes, detect_contradictions, compute_deltas_over_time
-from render_brief import render_brief
-from pdf_export import brief_to_pdf_bytes
-
+from bugs import (
+    init_bugs_table,
+    create_bug,
+    list_bugs,
+    get_bug,
+    update_bug,
+    BUG_STATUSES,
+    SEVERITIES,
+    bug_metrics,
+)
 
 # -------------------- CONFIG --------------------
 st.set_page_config(page_title="SeeUs MVP", layout="centered")
